bc3ac2dcb0ad3bc404d833b40baf87be66f317fb
Thinkman Wang <wangxf1985@gmail.com>
2019-05-27 17:57:37 +0800

    update

--- pythinkutils/common/MD5Utils.py
+++ pythinkutils/common/MD5Utils.py
@@ -14,5 +14,24 @@ class MD5Utils(object):
         print(m.hexdigest())
         return m.hexdigest()
 
+    @classmethod
+    def md5_file(cls, szFilePath):
+        if not os.path.isfile(szFilePath):
+            return ""
+
+        myhash = hashlib.md5()
+        f = open(szFilePath, 'rb')
+
+        while True:
+            b = f.read(8096)
+            if not b:
+                break
+
+            myhash.update(b)
+
+        f.close()
+        return myhash.hexdigest()
+
 
 # print(MD5Utils.md5("123456").lower())
+print(MD5Utils.md5_file("log.py"))
