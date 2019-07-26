f2ef7743ef12cd8abc7e7e058716bfe8d3f1af7d
Thinkman Wang <wangxf1985@gmail.com>
2019-07-26 17:17:36 +0800

    update

--- pythinkutils/config/Config.py
+++ pythinkutils/config/Config.py
@@ -44,19 +44,10 @@ class ThinkConfig:
     @classmethod
     def get_default_config(cls):
         if cls.m_myConfig is None:
-            parser = argparse.ArgumentParser()
-            parser.add_argument("--env", type=str, default="pro", help="--env prd/dev")
-            args = parser.parse_args()
-            # print(args)
-
             config = ThinkConfig()
-            if "dev" == args.env:
-                config.read(os.path.dirname(os.path.abspath(__file__)) + "/app_dev.properties")
-            else:
-                config.read(os.path.dirname(os.path.abspath(__file__)) + "/app.properties")
+            config.read(os.path.dirname(os.path.abspath(__file__)) + "/app.properties")
 
             cls.m_myConfig = config
-
 
         return cls.m_myConfig
 
