6ac52b7af15cda766b9f334cebe3101f96587ff1
Thinkman Wang <wangxf1985@gmail.com>
2019-08-19 18:54:00 +0800

    update

---
+++ pythinkutils/aio/kafka/test/kafka_receive_test.py
@@ -0,0 +1,31 @@
+# -*- coding: UTF-8 -*-
+
+import sys
+import os
+
+import asyncio
+from pythinkutils.aio.kafka.ThinkAioKafkaConsumer import ThinkAioKafkaConsumer
+from pythinkutils.config.Config import g_config
+from pythinkutils.aio.common.aiolog import g_aio_logger
+
+class TestConsumer(ThinkAioKafkaConsumer):
+
+    def __init__(self, szHost, szTopic, szGroup):
+        super().__init__(szHost, szTopic, szGroup)
+
+    async def on_task(self, msg):
+        # await asyncio.sleep(5)
+        # random.randint
+        # print("consumed: ", msg.topic, msg.partition, msg.offset, msg.key, msg.value, msg.timestamp)
+        szMsg = str(msg.value, "utf-8")
+        g_aio_logger.info(szMsg)
+
+
+async def main():
+    myConsumer = TestConsumer(g_config.get("kafka", "host"), g_config.get("kafka", "topic"), "myGroup")
+    myConsumer.start()
+
+    asyncio.get_running_loop().run_forever()
+
+if __name__ == '__main__':
+    asyncio.run(main())
