4b3c55bbb369bb85ad2ae2a68ff760a84aa7a8e2
Thinkman Wang <wangxf1985@gmail.com>
2020-07-16 20:26:31 +0800

    update

--- test/tornado/MyEchoClient.py
+++ test/tornado/MyEchoClient.py
@@ -9,39 +9,60 @@ from tornado.tcpserver import TCPServer
 from tornado.tcpclient import TCPClient
 from tornado.iostream import StreamClosedError
 from tornado import gen
+import random
 
 # from pythinkutils.aio.common.aiolog import g_aio_logger
 
 ENCODING = "utf-8"
 EOF = b"\r\n"
 
-async def echo(stream, text):
-    if text is None or len(text.strip()) <= 0:
-        return
 
-    byteStr = text.encode(ENCODING)
-    # print(type(text))
-    # print(type(byteStr))
+async def echo(stream):
+    print("start echo")
+    # while stream is not None \
+    #         and stream.closed() is False:
+    while True:
+        try:
+            byteData = await stream.read_until(EOF)
+            szText = byteData.decode(ENCODING)
 
-    await stream.write(text.encode(ENCODING))
-    await stream.write(EOF)
+            print("reply>>> " + szText)
 
-    byteData = await stream.read_until(EOF)
-    szText = byteData.decode(ENCODING)
-    print("reply>>> " + szText)
+        except Exception as ex:
+            pass
 
-async def run_client():
-    stream = await TCPClient().connect('127.0.0.1', 8888)
+async def test1():
+    while True:
+        await asyncio.sleep(1)
+        print("FXXK")
+
+async def send(stream):
+    print("start send")
+    asyncio.ensure_future(test1())
+
     try:
         while True:
-            szData = input('echo>>> ')
-            await echo(stream, szData)
+            await asyncio.sleep(1)
+            szData = str(random.randint(1, 100))
+            print("send>>> " + szData)
+
+            if szData is None or len(szData.strip()) <= 0:
+                return
+
+            await stream.write(szData.encode(ENCODING))
+            await stream.write(EOF)
+
     except KeyboardInterrupt:
         stream.close()
 
+async def run_client():
+    stream = await TCPClient().connect('127.0.0.1', 8888)
+    await asyncio.gather(send(stream), echo(stream))
 
-async def main():
-    await run_client()
+def main():
+    loop = asyncio.get_event_loop()
+    asyncio.gather(run_client())
+    loop.run_forever()
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    main()
