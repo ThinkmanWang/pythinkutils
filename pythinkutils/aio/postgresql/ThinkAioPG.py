e77f4437c2b5a2c8a77d4910e5747be97eee0167
Thinkman Wang <wangxf1985@gmail.com>
2020-08-06 19:01:40 +0800

    update

--- pythinkutils/aio/postgresql/ThinkAioPG.py
+++ pythinkutils/aio/postgresql/ThinkAioPG.py
@@ -75,7 +75,7 @@ class ThinkAioPG(object):
 #
 #         print(ret)
 #
-#         await cur.execute("INSERT INTO t_test_col(id, name, score) VALUES (%s, %s, %s)", (1024001, "Thinkman Wang", 99))
+#         # await cur.execute("INSERT INTO t_test_col(id, name, score) VALUES (%s, %s, %s)", (1024001, "Thinkman Wang", 99))
 #
 # if __name__ == '__main__':
 #     loop = asyncio.get_event_loop()
