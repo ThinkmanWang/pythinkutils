9d51424a56d67aad1a74e54f77cfa31ca6f9d324
Thinkman Wang <wangxf1985@gmail.com>
2020-08-07 20:35:17 +0800

    update

--- pythinkutils/common/datetime_utils.py
+++ pythinkutils/common/datetime_utils.py
@@ -173,3 +173,34 @@ datetime.date(2002, 1, 31)
 # for i in range(100):
 #     szText = "{} {}".format(i + 1, start_end_of_week(2019, i))
 #     print(szText)
+
+
+import random
+import time
+from datetime import datetime
+
+def strTimeProp(start, end, prop, frmt):
+    stime = time.mktime(time.strptime(start, frmt))
+    etime = time.mktime(time.strptime(end, frmt))
+    ptime = stime + prop * (etime - stime)
+    return int(ptime)
+
+def randomTimestamp(start, end, frmt='%Y-%m-%d %H:%M:%S'):
+    return strTimeProp(start, end, random.random(), frmt)
+
+def randomDate(start, end, frmt='%Y-%m-%d %H:%M:%S'):
+    return time.strftime(frmt, time.localtime(strTimeProp(start, end, random.random(), frmt)))
+
+def randomTimestampList(start, end, n, frmt='%Y-%m-%d %H:%M:%S'):
+    return [randomTimestamp(start, end, frmt) for _ in range(n)]
+
+def randomDateList(start, end, n, frmt='%Y-%m-%d %H:%M:%S'):
+    return [randomDate(start, end, frmt) for _ in range(n)]
+
+# start = '2018-06-02 12:12:12'
+# end = '2018-11-01 00:00:00'
+# lenth = 10
+# print(randomTimestamp(start, end))
+# print(randomDate(start,end))
+# print(randomTimestampList(start, end, lenth))
+# print(randomDateList(start, end, lenth))
