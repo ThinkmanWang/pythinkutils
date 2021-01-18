bd401afc0ee811e7128218a2ee1b340fc8f06c35
Thinkman Wang <wangxf1985@gmail.com>
2021-01-18 11:47:21 +0800

    update

--- pythinkutils/common/AjaxResult.py
+++ pythinkutils/common/AjaxResult.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 
+from pythinkutils.common.object2json import obj2json
+
 class AjaxResult:
-    def __init__(self, code=200, msg="", data=None):
+    def __init__(self, code=200, msg="success", data=None):
         self.code = code
         self.msg = msg
         self.data = data
@@ -9,10 +11,10 @@ class AjaxResult:
     @classmethod
     def error(cls, msg = None):
         if msg is None:
-            return AjaxResult(500, "Server Error")
+            return obj2json(AjaxResult(500, "Server Error"))
         else:
-            return AjaxResult(500, msg)
+            return obj2json(AjaxResult(500, msg))
 
     @classmethod
     def success(cls, data):
-        return AjaxResult(data=data)
+        return obj2json(AjaxResult(data=data))
